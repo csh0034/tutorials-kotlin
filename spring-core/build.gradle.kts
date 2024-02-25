c269df4eb14b370914f102b46ab2906fdc1b5794
csh0034 <csh0034@gmail.com>
2024-02-25 22:42:32 +0900

    chore(spring-core): 붎필요한 의존성 제거
    - kotlin-reflect 하위에 kotlin-stdlib-jdk8 포함

--- spring-core/build.gradle.kts
+++ spring-core/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("org.jetbrains.kotlin:kotlin-reflect")
-  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   runtimeOnly("com.h2database:h2")
   implementation("com.querydsl:querydsl-jpa:${dependencyManagement.importedProperties["querydsl.version"]}:jakarta")

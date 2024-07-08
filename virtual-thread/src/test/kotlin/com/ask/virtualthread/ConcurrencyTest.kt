4919b61cbc092428f1b47baf5ce31f1eb3500474
csh0034 <csh0034@gmail.com>
2024-07-08 15:24:26 +0900

    test(virtual-thread): Lock 코틀린 함수

--- virtual-thread/src/test/kotlin/com/ask/virtualthread/ConcurrencyTest.kt
+++ virtual-thread/src/test/kotlin/com/ask/virtualthread/ConcurrencyTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
 import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class ConcurrencyTest {
 
@@ -16,13 +17,32 @@ class ConcurrencyTest {
     Executors.newVirtualThreadPerTaskExecutor().use {
       (1..100).forEach { _ ->
         it.execute {
-          lock.lock()
           try {
             val tmp = count
             Thread.sleep(1)
             count = tmp + 1
           } finally {
             lock.unlock()
+          }
+        }
+      }
+    }
+
+    assertThat(count).isEqualTo(100)
+  }
+
+  @Test
+  fun `reentrantLock with kotlin function`() {
+    val lock = ReentrantLock()
+    var count = 0
+
+    Executors.newVirtualThreadPerTaskExecutor().use {
+      (1..100).forEach { _ ->
+        it.execute {
+          lock.withLock {
+            val tmp = count
+            Thread.sleep(1)
+            count = tmp + 1
           }
         }
       }

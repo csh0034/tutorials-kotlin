8145ac0ad1b9c87598b0df02912013d50b5ca98a
csh0034 <csh0034@gmail.com>
2024-08-14 11:36:40 +0900

    feat(error-handling): init

---
+++ error-handling/src/test/kotlin/com/ask/errorhandling/ErrorHandlingApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.ask.errorhandling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ErrorHandlingApplicationTests {
+  @Test
+  fun contextLoads() {
+  }
+}

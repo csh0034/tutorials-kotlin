3826c73771cb0c47661f8e66a847a9f99b707914
csh0034 <csh0034@gmail.com>
2024-10-15 13:23:36 +0900

    test(spring-jpa): Repository query keywords

--- spring-jpa/src/main/kotlin/com/ask/jpa/vote/VoteRepository.kt
+++ spring-jpa/src/main/kotlin/com/ask/jpa/vote/VoteRepository.kt
@@ -2,10 +2,18 @@ package com.ask.jpa.vote;
 
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.transaction.annotation.Transactional
+
 
 interface VoteRepository : JpaRepository<Vote, String> {
   @EntityGraph(attributePaths = ["voteItems"])
   fun findWithVoteItemsById(id: String): Vote?
+
+  @Transactional
+  fun removeByTitle(title: String): Int
+
+  @Transactional
+  fun deleteByTitle(title: String): Int
 }
 
 fun VoteRepository.getWithVoteItems(id: String) = findWithVoteItemsById(id) ?: throw IllegalStateException("vote not found")
--- spring-jpa/src/test/kotlin/com/ask/jpa/vote/VoteRepositoryTest.kt
+++ spring-jpa/src/test/kotlin/com/ask/jpa/vote/VoteRepositoryTest.kt
@@ -3,13 +3,17 @@ package com.ask.jpa.vote
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
 
 @SpringBootTest
 class VoteRepositoryTest {
+  private val log = LoggerFactory.getLogger(javaClass)
+
   @Autowired
   lateinit var voteRepository: VoteRepository
 
@@ -40,5 +44,26 @@ class VoteRepositoryTest {
   fun findFetchById() {
     val vote = voteRepository.getWithVoteItems(voteId)
     assertThat(vote).isNotNull
+  }
+
+  /**
+   * @see <a href="https://docs.spring.io/spring-data/redis/reference/data-commons/repositories/query-keywords-reference.html">Repository query keywords</>
+   */
+  @DisplayName("query method 사용하므로 entity 조회후 delete 호출함, remove*")
+  @Test
+  fun removeByTitle() {
+    log.debug("removeByTitle start")
+    val result = voteRepository.removeByTitle("vote")
+    assertThat(result).isOne()
+    log.debug("removeByTitle end")
+  }
+
+  @DisplayName("query method 사용하므로 entity 조회후 delete 호출함, delete*")
+  @Test
+  fun deleteByTitle() {
+    log.debug("deleteByTitle start")
+    val result = voteRepository.deleteByTitle("vote")
+    assertThat(result).isOne()
+    log.debug("deleteByTitle end")
   }
 }

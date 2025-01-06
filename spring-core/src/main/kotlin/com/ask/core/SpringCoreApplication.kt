490ad68977f72943f7ed9d91124da5b6fe16fd7c
csh0034 <csh0034@gmail.com>
2025-01-06 17:51:51 +0900

    feat(spring-core):Bean Definition DSL

--- spring-core/src/main/kotlin/com/ask/core/SpringCoreApplication.kt
+++ spring-core/src/main/kotlin/com/ask/core/SpringCoreApplication.kt
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.support.beans
+import org.springframework.web.servlet.function.router
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -19,6 +21,19 @@ class SpringCoreApplication {
   }
 }
 
+val beans = beans {
+  bean("word") { "Test" }
+  bean(::route)
+}
+
+fun route(word: String) = router {
+  "/route".nest {
+    GET("") { _ -> ok().body(word) }
+  }
+}
+
 fun main(args: Array<String>) {
-  runApplication<SpringCoreApplication>(*args)
+  runApplication<SpringCoreApplication>(*args) {
+    addInitializers(beans)
+  }
 }

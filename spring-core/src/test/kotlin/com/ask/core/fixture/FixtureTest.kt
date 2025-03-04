3808ab3c9216467833d5fbb59bfc1d751420e928
csh0034 <csh0034@gmail.com>
2025-03-04 11:54:43 +0900

    test(spring-core): data class fixture copy
    - copy 는 shallow copy 로 동작함

---
+++ spring-core/src/test/kotlin/com/ask/core/fixture/FixtureTest.kt
@@ -0,0 +1,39 @@
+package com.ask.core.fixture
+
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
+import org.assertj.core.api.Assertions.assertThatNoException
+import org.junit.jupiter.api.Test
+
+class FixtureTest {
+  @Test
+  fun `나이가 18세 미만이면 실패한다`() {
+    val invalidAge = 17
+
+    assertThatIllegalArgumentException().isThrownBy {
+      validUserInfo.copy(age = invalidAge)
+    }
+  }
+
+  @Test
+  fun `나이가 18세 이상이면 성공한다`() {
+    val validAge = 18
+
+    assertThatNoException().isThrownBy {
+      validUserInfo.copy(age = validAge)
+    }
+  }
+
+  private val validUserInfo = UserInfo(
+    name = "ask",
+    age = 30,
+  )
+}
+
+private data class UserInfo(
+  val name: String,
+  val age: Int,
+) {
+  init {
+    require(age >= 18)
+  }
+}

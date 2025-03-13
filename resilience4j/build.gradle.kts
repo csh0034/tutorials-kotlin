9e8ac130013ec64d6c166823140433799e6494cf
csh0034 <csh0034@gmail.com>
2025-03-13 15:49:45 +0900

    feat(resilience4j): actuator 설정 추가

--- resilience4j/build.gradle.kts
+++ resilience4j/build.gradle.kts
@@ -23,6 +23,7 @@ extra["springCloudVersion"] = "2024.0.0"
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 //	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")

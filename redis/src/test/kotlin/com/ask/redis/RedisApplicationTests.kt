3d1467d0a0c664005d5a653c0b3d397578c20471
csh0034 <csh0034@gmail.com>
2025-04-01 10:33:56 +0900

    feat(redis): init

---
+++ redis/src/test/kotlin/com/ask/redis/RedisApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.ask.redis
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedisApplicationTests {
+  @Test
+  fun contextLoads() {
+  }
+}

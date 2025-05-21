291890b4d715f140dfdc86709bc0087d15c89188
csh0034 <csh0034@gmail.com>
2025-05-21 17:37:55 +0900

    test(coroutine): CoroutineTest

---
+++ coroutine/src/test/kotlin/com/ask/coroutine/CoroutineTest.kt
@@ -0,0 +1,94 @@
+package com.ask.coroutine
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import kotlin.system.measureTimeMillis
+
+class CoroutineTest {
+  @Test
+  fun `delay Dispatchers IO`() = runBlocking {
+    val time = measureTimeMillis {
+      val deferred1 = async(Dispatchers.IO) { delayData() }
+      val deferred2 = async(Dispatchers.IO) { delayData() }
+      val deferred3 = async(Dispatchers.IO) { delayData() }
+
+      // 결과 수집
+      val result1 = deferred1.await()
+      val result2 = deferred2.await()
+      val result3 = deferred3.await()
+
+      println("Results: $result1, $result2, $result3")
+    }
+
+    println("Elapsed time: ${time}ms")
+  }
+
+  @Test
+  fun `delay EmptyCoroutineContext`() {
+    val test = runBlocking {
+      val deferred1 = async { delayData() }
+      val deferred2 = async { delayData() }
+      val deferred3 = async { delayData() }
+
+      // 결과 수집
+      val result1 = deferred1.await()
+      val result2 = deferred2.await()
+      val result3 = deferred3.await()
+
+      "Results: $result1, $result2, $result3"
+    }
+
+    println(test)
+  }
+
+  @Test
+  fun `sleep Dispatchers IO`() = runBlocking {
+    val time = measureTimeMillis {
+      val deferred1 = async(Dispatchers.IO) { sleepData() }
+      val deferred2 = async(Dispatchers.IO) { sleepData() }
+      val deferred3 = async(Dispatchers.IO) { sleepData() }
+
+      // 결과 수집
+      val result1 = deferred1.await()
+      val result2 = deferred2.await()
+      val result3 = deferred3.await()
+
+      println("Results: $result1, $result2, $result3")
+    }
+
+    println("Elapsed time: ${time}ms")
+  }
+
+  @Test
+  fun `sleep EmptyCoroutineContext`() = runBlocking {
+    val time = measureTimeMillis {
+      val deferred1 = async { sleepData() }
+      val deferred2 = async { sleepData() }
+      val deferred3 = async { sleepData() }
+
+      // 결과 수집
+      val result1 = deferred1.await()
+      val result2 = deferred2.await()
+      val result3 = deferred3.await()
+
+      println("Results: $result1, $result2, $result3")
+    }
+
+    println("Elapsed time: ${time}ms")
+  }
+
+  suspend fun delayData(): String {
+    delay(1000)
+    println(Thread.currentThread().name)
+    return "Hello, Coroutine!"
+  }
+
+  fun sleepData(): String {
+    Thread.sleep(1000)
+    println(Thread.currentThread().name)
+    return "Hello, Coroutine!"
+  }
+}

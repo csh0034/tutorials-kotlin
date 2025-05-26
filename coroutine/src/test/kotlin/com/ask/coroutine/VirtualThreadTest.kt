708837705bd4323fd7f0604227e3882f8b530c80
csh0034 <csh0034@gmail.com>
2025-05-26 11:24:30 +0900

    test(coroutine): AsyncScope

--- coroutine/src/test/kotlin/com/ask/coroutine/VirtualThreadTest.kt
+++ coroutine/src/test/kotlin/com/ask/coroutine/VirtualThreadTest.kt
@@ -8,13 +8,15 @@ import kotlin.system.measureTimeMillis
 
 class VirtualThreadTest {
   @Test
-  fun runVT() {
+  fun awaitAll() {
     val time = measureTimeMillis {
-      val results = awaitAll {
-        async { Thread.sleep(1000) }
-        async { Thread.sleep(1000) }
-        async { Thread.sleep(1000) }
+      val results = awaitAll<Int> {
+        async { Thread.sleep(1000); 1 }
+        async { Thread.sleep(1000); 2 }
+        async { Thread.sleep(1000); 3 }
       }
+
+      results.map { it.getOrNull() }.forEach { println(it) }
 
       assertThat(results.map { it.isSuccess }).hasSize(3)
     }
@@ -22,20 +24,58 @@ class VirtualThreadTest {
     println("Elapsed time: ${time}ms")
     assertThat(time).isLessThan(1500)
   }
+
+  @Test
+  fun awaitAllUnit() {
+    val time = measureTimeMillis {
+      val results = awaitAllUnit {
+        async { Thread.sleep(1000) }
+        async { Thread.sleep(1000) }
+        async { Thread.sleep(1000) }
+      }
+
+      results.map { it.getOrNull() }.forEach { println(it) }
+
+      assertThat(results.map { it.isSuccess }).hasSize(3)
+    }
+
+    println("Elapsed time: ${time}ms")
+    assertThat(time).isLessThan(1500)
+  }
+
+  @Test
+  fun asyncScope() {
+    val time = measureTimeMillis {
+      val future1 = AsyncScope().async { Thread.sleep(1000) }
+      val future2 = AsyncScope().async { Thread.sleep(1000) }
+
+      future1.await()
+      future2.await()
+    }
+
+    assertThat(time).isLessThan(1500)
+  }
 }
 
 private val virtualExecutor = Executors.newVirtualThreadPerTaskExecutor()
 
-fun <T> awaitAll(block: AsyncCollector<T>.() -> Unit): List<Result<T>> {
-  val collector = AsyncCollector<T>()
-  collector.block()
-  return collector.futures.map { runCatching { it.get() } }
+fun <T> Future<T>.await(): T = this.get()
+
+@Suppress("UNCHECKED_CAST")
+fun <T> awaitAll(block: AsyncScope.() -> Unit): List<Result<T>> {
+  val scope = AsyncScope()
+  scope.block()
+  return scope.futures.map { runCatching { it.await() as T } }
 }
 
-class AsyncCollector<T> {
-  val futures = mutableListOf<Future<T>>()
+fun awaitAllUnit(block: AsyncScope.() -> Unit) = awaitAll<Unit>(block)
 
-  fun async(action: () -> T) {
-    futures += virtualExecutor.submit(action)
+class AsyncScope {
+  val futures = mutableListOf<Future<*>>()
+
+  fun <T> async(action: () -> T): Future<T> {
+    val future = virtualExecutor.submit(action)
+    futures += future
+    return future
   }
 }

9cf8e858255ba56cf7812d20d3cc0d0c7bba76d1
csh0034 <csh0034@gmail.com>
2025-07-09 13:13:09 +0900

    feat(compress): init

---
+++ compress/src/main/kotlin/com/ask/compress/CompressApplication.kt
@@ -0,0 +1,11 @@
+package com.ask.compress
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CompressApplication
+
+fun main(args: Array<String>) {
+  runApplication<CompressApplication>(*args)
+}
---
+++ compress/src/test/kotlin/com/ask/compress/CompressApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.ask.compress
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CompressApplicationTest {
+  @Test
+  fun contextLoads() {
+  }
+}

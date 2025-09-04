2d4cb7905c7352a4de55ab88ebc1ef9e257ca861
csh0034 <csh0034@gmail.com>
2025-09-04 18:21:04 +0900

    test(spring-ai): EmbeddingModel

---
+++ spring-ai/src/test/kotlin/com/ask/springai/EmbeddingModelTest.kt
@@ -0,0 +1,21 @@
+package com.ask.springai
+
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.ai.embedding.EmbeddingModel
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmbeddingModelTest {
+  private val log = LoggerFactory.getLogger(javaClass)
+
+  @Autowired
+  lateinit var embeddingModel: EmbeddingModel
+
+  @Test
+  fun embed() {
+    val embed = embeddingModel.embed("King")
+    log.info("embed: {}", embed)
+  }
+}

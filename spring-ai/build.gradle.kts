28236c64741c9c888959763fb8d683363ba7d455
csh0034 <csh0034@gmail.com>
2025-09-05 09:08:39 +0900

    chore(spring-ai): indent

--- spring-ai/build.gradle.kts
+++ spring-ai/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.5"
-    id("io.spring.dependency-management") version "1.1.7"
+  kotlin("jvm") version "1.9.25"
+  kotlin("plugin.spring") version "1.9.25"
+  id("org.springframework.boot") version "3.5.5"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.ask"
@@ -10,38 +10,38 @@ version = "0.0.1-SNAPSHOT"
 description = "spring-ai"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 val springAiVersion = "1.0.1"
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springframework.ai:spring-ai-starter-model-openai")
-    implementation("org.springframework.ai:spring-ai-advisors-vector-store")
-    implementation("org.springframework.ai:spring-ai-starter-vector-store-chroma")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.ai:spring-ai-starter-model-openai")
+  implementation("org.springframework.ai:spring-ai-advisors-vector-store")
+  implementation("org.springframework.ai:spring-ai-starter-vector-store-chroma")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 dependencyManagement {
-    imports {
-        mavenBom("org.springframework.ai:spring-ai-bom:${springAiVersion}")
-    }
+  imports {
+    mavenBom("org.springframework.ai:spring-ai-bom:${springAiVersion}")
+  }
 }
 
 kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
+  compilerOptions {
+    freeCompilerArgs.addAll("-Xjsr305=strict")
+  }
 }
 
 tasks {

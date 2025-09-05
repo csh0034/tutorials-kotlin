d50b969e8b381cc4379b1124ce71a8036327379d
csh0034 <csh0034@gmail.com>
2025-09-05 11:34:49 +0900

    test(spring-ai): metadata filtering

--- spring-ai/src/test/kotlin/com/ask/springai/VectorStoreTest.kt
+++ spring-ai/src/test/kotlin/com/ask/springai/VectorStoreTest.kt
@@ -16,16 +16,24 @@ class VectorStoreTest {
   lateinit var vectorStore: VectorStore
 
   @Test
+  fun delete() {
+    vectorStore.delete("meta in ['meta1', 'meta2', 'meta3']")
+  }
+
+  @Test
   fun add() {
     val documents = listOf(
       Document(
         "Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!! Spring AI rocks!!",
-        mapOf("meta1" to "meta1")
+        mapOf("meta" to "meta1"),
       ),
-      Document("The World is Big and Salvation Lurks Around the Corner"),
+      Document(
+        "The World is Big and Salvation Lurks Around the Corner",
+        mapOf("meta" to "meta2"),
+      ),
       Document(
         "You walk forward facing the past and you turn back toward the future.",
-        mapOf("meta2" to "meta2")
+        mapOf("meta" to "meta3"),
       )
     )
 

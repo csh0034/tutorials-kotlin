63419658eaea3f59b7bce8853ed01a34069f90ca
csh0034 <csh0034@gmail.com>
2025-09-22 15:11:27 +0900

    style(spring-ai): import 변경

--- spring-ai/src/main/kotlin/com/ask/springai/ui/EtlController.kt
+++ spring-ai/src/main/kotlin/com/ask/springai/ui/EtlController.kt
@@ -3,7 +3,11 @@ package com.ask.springai.ui
 import org.springframework.ai.reader.tika.TikaDocumentReader
 import org.springframework.ai.transformer.splitter.TokenTextSplitter
 import org.springframework.ai.vectorstore.VectorStore
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController

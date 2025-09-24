5147ab25c72c4736d8969562bf676ae28665a6bc
csh0034 <csh0034@gmail.com>
2025-09-24 15:46:34 +0900

    refactor(spring-ai-mcp-server): McpSyncServerExchange

--- spring-ai-mcp-server/src/main/kotlin/com/ask/springaimcpserver/tool/DateTimeTool.kt
+++ spring-ai-mcp-server/src/main/kotlin/com/ask/springaimcpserver/tool/DateTimeTool.kt
@@ -2,6 +2,7 @@ package com.ask.springaimcpserver.tool
 
 import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.model.ToolContext
+import org.springframework.ai.mcp.McpToolUtils
 import org.springframework.ai.tool.annotation.Tool
 import org.springframework.ai.tool.annotation.ToolParam
 import org.springframework.context.i18n.LocaleContextHolder
@@ -21,9 +22,12 @@ class DateTimeTool {
   }
 
   @Tool(name = "set-alarm", description = "ISO-8601 형식으로 제공되는 주어진 시간 동안 사용자 알람 설정합니다.")
-  fun setAlarm(@ToolParam(description = "ISO-8601 형식의 시간") time: String) {
+  fun setAlarm(@ToolParam(description = "ISO-8601 형식의 시간") time: String, toolContext: ToolContext) {
     val alarmTime = LocalDateTime.parse(time, DateTimeFormatter.ISO_DATE_TIME)
 
     log.info("Alarm set for {}", alarmTime)
+
+    val mcpExchange = McpToolUtils.getMcpExchange(toolContext).orElseThrow()
+    // todo ...
   }
 }

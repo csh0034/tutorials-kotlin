d7b938f25f983fc9e576addf3d711d5dc83e586f
csh0034 <csh0034@gmail.com>
2025-12-21 13:15:09 +0900

    feat(javers): init

---
+++ javers/src/test/kotlin/com/ask/javers/JaversApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.ask.javers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JaversApplicationTests {
+  @Test
+  fun contextLoads() {
+  }
+}
